3fb8eab409958fecf9eea33386352a2c1e703097
razar <razear007@gmail.com>
2021-06-07 09:20:57 -0400

    fixes Dockerfile.dev

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,17 @@
 #pushing docker images to docker hub
+docker push razear/mega-electric-web-app:$SHA
+docker push razear/mega-electric-web-app:latest
+
+docker push razear/mega-electric-app:latest
+docker push razear/mega-electric-app:$SHA
+
 docker push razear/mega-electric-nginx:latest
 docker push razear/mega-electric-nginx:$SHA
 
 docker push razear/mega-electric-mysql:latest
 docker push razear/mega-electric-mysql:$SHA
 
-docker push razear/mega-electric-web-app:$SHA
-docker push razear/mega-electric-web-app:latest
 
-docker push razear/mega-electric-app:latest
-docker push razear/mega-electric-app:$SHA
 
 #deploying to fandogh cloud
 fandogh service apply -f ./inventory-nginx/nginx-deployment.yml  -p SHA=$SHA
