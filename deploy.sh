bbc91c5716701143e056345e414f6f762ecfef3e
razar <razear007@gmail.com>
2021-06-07 11:04:29 -0400

    fixes travis build

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,11 @@ docker push razear/mega-electric-web-app:latest
 docker push razear/mega-electric-app:latest
 docker push razear/mega-electric-app:$SHA
 
-docker push razear/mega-electric-nginx:latest
-docker push razear/mega-electric-nginx:$SHA
-
 docker push razear/mega-electric-mysql:latest
 docker push razear/mega-electric-mysql:$SHA
 
-
+docker push razear/mega-electric-nginx:latest
+docker push razear/mega-electric-nginx:$SHA
 
 #deploying to fandogh cloud
 fandogh service apply -f ./inventory-nginx/nginx-deployment.yml  -p SHA=$SHA
